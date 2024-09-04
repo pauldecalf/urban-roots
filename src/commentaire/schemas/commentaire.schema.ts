a356afe81b6088dd4e8570224c63d1e01bfa597b
pauldecalf <70703497+pauldecalf@users.noreply.github.com>
2024-09-04 20:17:32 +0200

    update

--- src/commentaire/schemas/commentaire.schema.ts
+++ src/commentaire/schemas/commentaire.schema.ts
@@ -2,7 +2,7 @@ import { Schema } from 'mongoose';
 
 export const CommentaireSchema = new Schema({
     createdAt: { type: Date, default: Date.now },
-    createdBy: { type: String, required: true },
+    createdBy: { type: Schema.Types.ObjectId, ref: 'Utilisateurs', required: true },
     message: { type: String, required: true },
     publicationId: { type: Schema.Types.ObjectId, ref: 'Publication', required: true },
     commentaireId: { type: Schema.Types.ObjectId, ref: 'Commentaire' },
